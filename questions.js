2666dca4df2af9b61e9fbc05ce838c772d3e7ade
Mat Harden <matharden@gmail.com>
2017-07-17 10:20:13 +0100

    Turn into treasure hunt

--- questions.js
+++ questions.js
@@ -1,6 +1,35 @@
 var questions = [
-  [10,'/',2],
+  [10,'/',2,"this is the next clue"],
   [2,'*',5],
   [4,'+',3],
   [4,'*',3]
 ];
+
+var codedquestions1 = {
+  4561: [10,'/',2,"Hair band box"],
+  7423: [3,'+',2,"Get in a bag and go to sleep."],
+  8437: [3,'*',5,"In the sock darwer"],
+  9215: [4,'+',3,"Daddy strums this"],
+  7856: [13,'*',2,"Under grey chair"],
+  3276: [7,'+',4,"Rest your sleepy head little boy"],
+  5729: [12,'/',3,"Pots and pans"],
+  1388: [4,'+',4,"This game is aMAZing!"],
+  2486: [7,'*',5,"Where is your football kit?!"]
+};
+
+var codedquestions = {
+  1243: [1,'+',2,"Under desk"],
+  1379: [2,'+',2,"Baby seat"],
+  2984: [3,'+',2,"Ask Google: what does laundry mean?"],
+  3478: [3,'+',3,"Under bonky"],
+  4191: [2,'+',4,"Hair dryer"],
+  5823: [1,'+',5,"Kitchen door"],
+  6412: [3,'+',4,"Lego head"],
+  1000: [43,'+',17,"Another kind of Smartie"],
+  1141: [5,'*',5,"Under Mummy!"],
+  2275: [8,'*',3,"& cushion"],
+  3939: [10,'*',11,"Spare room pillow"],
+  4155: [9,'*',4,"Handy towels"],
+  6271: [7,'*',7,"Under highest sink"],
+  7812: [5,'*',9,"The eggs are cooking!"]
+};
--- quiz.js
+++ quiz.js
@@ -1,14 +1,18 @@
-var level = 0;
+let currentQuestion;
 
-var one = document.querySelector('#one');
-var two = document.querySelector('#two');
-var operator = document.querySelector('#operator');
-var answer = document.querySelector('#answer');
-var quiz = document.querySelector('form');
-var result = document.querySelector('#result');
-var step = document.querySelector('#step');
+const messege = document.querySelector('#messege');
+const nextclue = document.querySelector('#nextclue');
+const clue = document.querySelector('#clue');
+const codeform = document.querySelector('#codeform');
+const code = codeform.querySelector('#code');
+const puzzle = document.querySelector('#puzzle');
+const question = document.querySelector('#question');
+const one = puzzle.querySelector('#one');
+const two = puzzle.querySelector('#two');
+const operator = puzzle.querySelector('#operator');
+const answer = puzzle.querySelector('#answer');
 
-var formatOperator = function(q) {
+const formatOperator = q => {
   if (q === '*' || q.toLowerCase() === 'x') {
     return '&times;';
   } else if (q === '/') {
@@ -20,38 +24,77 @@ var formatOperator = function(q) {
   }
 };
 
-var setQuestion = function(question) {
+const setQuestion = question => {
   one.innerHTML = question[0];
   operator.innerHTML = formatOperator(question[1]);
   two.innerHTML = question[2];
-
-  step.innerHTML = (level + 1) + '/' + questions.length;
+  currentQuestion = question;
+  answer.value = '';
 };
 
-var calculate = {
-  '+': function(x, y) { return x + y },
-  '-': function(x, y) { return x - y },
-  '*': function(x, y) { return x * y },
-  '/': function(x, y) { return x / y }
+const calculate = {
+  '+': (x, y) => x + y,
+  '-': (x, y) => x - y,
+  '*': (x, y) => x * y,
+  '/': (x, y) => x / y
 };
 
-var product = function(q) {
-  return calculate[q[1]](q[0], q[2]);
+const product = q => calculate[q[1]](q[0], q[2]);
+
+const showMsg = msg => {
+  messege.innerText = msg;
+  messege.classList.add('show');
+  setTimeout(() => messege.classList.remove('show'), 3000);
 };
 
-quiz.addEventListener('submit', function(e){
+const setMode = mode => {
+  switch (mode) {
+    case 1:
+      // Show code.
+      codeform.removeAttribute('hidden');
+      nextclue.setAttribute('hidden', 1);
+      puzzle.setAttribute('hidden', 1);
+      code.focus();
+      break;
+    case 2:
+      // Show puzzle.
+      codeform.setAttribute('hidden', 1);
+      nextclue.setAttribute('hidden', 1);
+      puzzle.removeAttribute('hidden');
+      answer.focus();
+      break;
+    case 3:
+      // Show clue.
+      codeform.removeAttribute('hidden');
+      nextclue.removeAttribute('hidden');
+      puzzle.setAttribute('hidden', 1);
+      code.value = '';
+      code.focus();
+      break;
+  }
+};
+
+puzzle.addEventListener('submit', e => {
   e.preventDefault();
-  if (product(questions[level]) === parseInt(answer.value)) {
-    result.innerHTML = 'Correct';
-    level++;
-    if (level < questions.length) {
-      setQuestion(questions[level]);
-    } else {
-      result.innerHTML = 'You have reached the end of the quiz.'
-    }
+  if (product(currentQuestion) === parseInt(answer.value)) {
+    clue.innerHTML = codedquestions[code.value][3];
+    setMode(3);
   } else {
-    result.innerHTML = 'Wrong';
+    answer.select();
+    showMsg('incorrect');
   }
 });
 
-setQuestion(questions[level]);
+codeform.addEventListener('submit', e => {
+  e.preventDefault();
+  if (codedquestions.hasOwnProperty(code.value)) {
+    setQuestion(codedquestions[code.value]);
+    setMode(2);
+  } else {
+    code.select();
+    showMsg('incorrect code');
+  }
+});
+
+setMode(1);
+// setQuestion(questions[level]);
